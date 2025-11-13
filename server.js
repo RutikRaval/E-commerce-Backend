c2a012584d214e0927d784c0b3b9e6c6b4786fc9
Raj5656 <tailorraj108@gmail.com>
2025-11-13 17:21:16 +0530

    commit

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app=require('./src/app')
 const port=process.env.PORT || 5000
 
 app.listen(port,()=>{
-    console.log("server started at port",port);
+    console.log("server started at port number",port);
     
 })
